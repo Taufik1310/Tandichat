d2006586840cfa85be857c5f5870aef3d655e535
andiputraw <andiputraw925@gmail.com>
2023-06-02 16:04:52 +0700

    implemented cors

--- server/run.sh
+++ server/run.sh
@@ -3,17 +3,19 @@
 
 TAG="$1"
 
-if [ "$TAG" == "-docker-build" ]; then
+if [ "$TAG" == "--docker-build" ]; then
   # Build Docker image
   docker build -t realtime-postgres .
 
+  docker container create -p 5432:5432 
+
   # Run the container
-  docker run -d -p 5432:5432 realtime-postgres
+  docker container start -p 5432:5432 realtime-postgres
 fi
 
-if [ "$TAG" == "-docker-run" ]; then
+if [ "$TAG" == "--docker-run" ]; then
   # Run the container
-  docker run -d -p 5432:5432 realtime-postgres
+  docker container start  -p 5432:5432 realtime-postgres
 fi
 
 sleep 3
