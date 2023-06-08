63a1f47d5f8dc3fdbca118c04cec5214fbe0ed9e
andiputraw <andiputraw925@gmail.com>
2023-06-08 19:48:53 +0700

    fix cors issue at /api/logout

--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ require gorm.io/driver/mysql v1.5.1
 require (
 	github.com/bytedance/sonic v1.8.8 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.0 // indirect
 	github.com/go-jose/go-jose v2.6.1+incompatible // indirect
--- server/run.sh
+++ server/run.sh
@@ -3,19 +3,19 @@
 
 TAG="$1"
 
-if [ "$TAG" == "--docker-build" ]; then
+if [ "$TAG" == "image" ]; then
   # Build Docker image
   docker build -t realtime-postgres .
 
   docker container create -p 5432:5432 tandichat
 
   # Run the container
-  docker container start -p 5432:5432 tandichat
+  docker container start tandichat
 fi
 
-if [ "$TAG" == "--docker-run" ]; then
+if [ "$TAG" == "container" ]; then
   # Run the container
-  docker container start  -p 5432:5432 tandichat
+  docker container start tandichat
 fi
 
 sleep 3
